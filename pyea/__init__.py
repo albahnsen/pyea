37966e206e784c812b450b3edc4cb1fefece2fff
Al.Bahnsen <al.bahnsen@gmail.com>
2014-08-15 20:23:59 +0200

    add doc

--- pyea/__init__.py
+++ pyea/__init__.py
@@ -1,1 +1,13 @@
-__author__ = 'al'
+"""
+Evolutionary Algorithms in Python
+==================================
+pyea is a set of pure python libraries including the most common evolutionary algorithms.
+"""
+
+# Authors: Alejandro Correa Bahnsen <al.bahnsen@gmail.com>
+# License: BSD 3 clause
+
+__version__ = '0.1'
+
+
+from models import *
