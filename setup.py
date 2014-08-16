9b1e8d41bf8cfff44dfe854f797378b4f6c49f59
Al.Bahnsen <al.bahnsen@gmail.com>
2014-08-16 15:32:39 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import re
+
+for line in open('pyea/__init__.py'):
+    match = re.match("__version__ *= *'(.*)'", line)
+    if match:
+        __version__, = match.groups()
+
+setup(name='pyea',
+      version=__version__,
+      description='Set of pure python libraries including the most common evolutionary algorithms',
+      long_description=open('README.md').read(),
+      author='Alejandro CORREA BAHNSEN',
+      author_email='al.bahnsen@gmail.com',
+      url='https://github.com/albahnsen/pyea',
+      license='new BSD',
+      packages=find_packages(),
+      keywords=['optimization', 'evolutionary', 'genetic'],
+      install_requires=['scikit-learn>=0.15.1','pandas>=0.14.0','numpy>=1.8.1'],
+      classifiers=['Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development',
+                   'Topic :: Scientific/Engineering',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',],
+      )
